14aaecfbeff2b97f94edc6f2c83227c4884b53cc
Yadickson Soto <yadickson@gmail.com>
2024-03-21 20:45:48 -0300

    Update test_layer.py

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,5 +1,7 @@
-import pytest
+from layer import Layer
+
 
 class TestLayer:
-  def test_create_layer(self):
-    pass
+  def test_should_return_one_number_of_neurons_when_create_layer_with_empty_parameters(self):
+    layer = Layer()
+    assert 1 == layer.number_of_neurons
