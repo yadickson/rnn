2f7cd5d1d24e618a83ee07412baeb8d858c353d8
Yadickson Soto <yadickson@gmail.com>
2024-03-25 01:18:36 -0300

    Update test_layer.py

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,8 +1,8 @@
 import unittest
-from layer import Layer
+import layer
 
 
 class TestLayer(unittest.TestCase):
   def test_should_return_one_number_of_neurons_when_create_layer_with_empty_parameters(self):
-    layer = Layer()
-    assert 1 == layer.number_of_neurons
+    object = layer.Layer()
+    assert 1 == object.number_of_neurons
